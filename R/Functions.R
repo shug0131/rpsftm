7c31d03afbd7b70735627a261d0c6a2d96ae54a6
unknown <sjb277@CTUPC03.medschl.cam.ac.uk>
2015-04-15 15:59:34 +0100

    change the interface to allow adjustors, for survdiff function.
    Need to add in other tests...
    
    Clean up use of data argument...
    
    put in a function to generate formula - question howto handle Arm,

--- R/Functions.R
+++ R/Functions.R
@@ -1,5 +1,7 @@
 Recensor=function(phi,Time,CensorTime,Rx){
   require(survival)
+  #Put in the data= argument and process.
+    
   if( any (!(0<=Rx & Rx<=1))){stop("Invalid values for Rx. Must be proportions in [0,1]")}
   if( any(CensorTime<Time)){warning("You have observed events AFTER censoring. These are handled as censored")}
   U=Time*((1-Rx)+Rx*exp(phi))
@@ -12,21 +14,60 @@ Recensor=function(phi,Time,CensorTime,Rx){
 # do a log-rank test
 # Could generalise this to other tests.
 #build a formula style interface
-Test=function(phi,Time,CensorTime,Rx,Arm, target=0){
+Test=function(phi,Time,CensorTime,Rx,Arm, data, adjustors, target=0, test, ...){
   require(survival)
   Sstar=Recensor( phi,Time,CensorTime,Rx)
+  data=cbind(Sstar, data)
   # needs a check that Arm is two levels
   if( length(unique(Arm))!=2){stop("Arm must have exactly 2 observed values")}
   # Might want to generalise to allow other tests, and adjustors.
-  fit=survdiff( Sstar~Arm)
-  z=(fit$exp-fit$obs)/sqrt(diag(fit$var))
-  fit$chisq
-  z[1]-target
+  #build a formula object,
+  if(is.null(adjustors)){
+    MyFormula=as.formula("~1")
+  }else{
+    MyFormula= as.formula(adjustors)
+  }
+  MyFormula=update(MyFormula, Sstar~.+Arm)
+  
+  # mycall=list(test, formula, data,...)
+  # mycall=as.call(mycall)
+  # fit=eval(mycall)
+  
+  
+  fit=survdiff( MyFormula,data)
+  
+  ExtractZ(fit)-target
+  
+}
+
+ExtractZ=function(x,...){UseMethod("ExtractZ")}
+ExtractZ.survdiff=function(fit){
+  if (is.matrix(fit$obs)) {
+    otmp <- apply(fit$obs, 1, sum)
+    etmp <- apply(fit$exp, 1, sum)
+  }
+  else {
+    otmp <- fit$obs
+    etmp <- fit$exp
+  }
+  df <- (etmp > 0)
+  if (sum(df) < 2) 
+    chi <- 0
+  else {
+    temp2 <- ((otmp - etmp)[df])[-1]
+    vv <- (fit$var[df, df])[-1, -1, drop = FALSE]
+    #chi <- sum(solve(vv, temp2) * temp2)
+    z<- temp2/sqrt(vv)
+  }
+  z
 }
 
 
 
-rpsftm=function(Time, CensorTime, Rx, Arm,data, lowphi=-10,hiphi=10, alpha=0.05){
+
+
+
+rpsftm=function(Time, CensorTime, Rx, Arm,data, adjustors=NULL, test=logrank, lowphi=-10,hiphi=10, alpha=0.05,...){
   #input
   # Time survival time/or censoring time
   # Rx proportion of time on treatment
@@ -53,12 +94,15 @@ rpsftm=function(Time, CensorTime, Rx, Arm,data, lowphi=-10,hiphi=10, alpha=0.05)
   #solve to find the value of phi that gives the root to z=0
   ans=uniroot(Test, c(lowphi,hiphi), 
               Time=Time, CensorTime=CensorTime, Rx=Rx, Arm=Arm, 
+              data=data, adjustors=adjustors,
               target=0)
   lower=uniroot(Test, c(lowphi,hiphi), 
                 Time=Time, CensorTime=CensorTime, Rx=Rx, Arm=Arm, 
+                data=data, adjustors=adjustors,
                 target=qnorm(1-alpha/2))
   upper=uniroot(Test, c(lowphi,hiphi), 
                 Time=Time, CensorTime=CensorTime, Rx=Rx, Arm=Arm, 
+                data=data, adjustors=adjustors,
                 target=qnorm(alpha/2))
   
 
@@ -66,7 +110,18 @@ rpsftm=function(Time, CensorTime, Rx, Arm,data, lowphi=-10,hiphi=10, alpha=0.05)
   phiHat=ans$root
   Sstar=Recensor(phiHat, Time, CensorTime,Rx)
   #not sure about this, maybe survfit(Sstart~1)??
-  fit=survfit(Sstar~Arm)
+  
+  #ought to make this a function to use in test as well
+  
+  if(is.null(adjustors)){
+    MyFormula=as.formula("~1")
+  }else{
+    MyFormula= as.formula(adjustors)
+  }
+  MyFormula=update(MyFormula, Sstar~.+Arm)
+  
+  
+  fit=survfit(MyFormula, data)
   
   
   list(phi=phiHat, 
