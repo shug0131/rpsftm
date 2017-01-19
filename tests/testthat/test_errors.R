96e8eac2bbaef30d4f87d47ac1cfa72344575f31
unknown <sjb277@CTUPC03.medschl.cam.ac.uk>
2017-01-19 17:02:28 +0000

    added code at bottom of test_error.R to try to produce errors in terms of NA in the randomised treatment arm... Bug issue 2 #2 in github

--- tests/testthat/test_errors.R
+++ tests/testthat/test_errors.R
@@ -178,3 +178,20 @@ test_that("eval_z incorrect input - non-numeric input",
             
           }
 )
+
+
+test_that("NA handling in rand",{
+imm2 <- immdef$imm
+imm2[1:100] <- NA
+rpsftm(Surv(progyrs, prog)~rand(imm2,1-xoyrs/progyrs),
+       data=immdef, censor_time = censyrs)
+imm3 <- factor(imm2, levels=0:1, labels=c("Cx","Rx"))
+rpsftm(Surv(progyrs, prog)~rand(imm3,1-xoyrs/progyrs),
+       data=immdef, censor_time = censyrs)
+expect_error(rpsftm(Surv(progyrs, prog)~rand(1-xoyrs/progyrs,imm),
+       data=immdef, censor_time = censyrs))
+imm_3arms <- factor(rep(1:3, ceiling(1000/3))[1:1000])
+expect_error(rpsftm(Surv(progyrs, prog)~rand(imm_3arms,1-xoyrs/progyrs),
+       data=immdef, censor_time = censyrs))
+}
+)
