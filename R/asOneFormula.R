4ac2b5dac4b83a62368dc8b31baf92d3fafbea4c
Simon Bond <sjb277@medschl.cam.ac.uk>
2019-01-10 14:11:24 +0000

    adding warnings to one_becomes_three

--- R/asOneFormula.R
+++ R/asOneFormula.R
@@ -18,23 +18,39 @@ allVarsRec <- function (object)
   }
 }
 
-one_becomes_three <- function(formula){
+one_becomes_three <- function(formula, env=parent.frame()){
   
   formula <- terms(formula, specials = "rand")
   var_list <- attr(formula, "variables")
   rand_index <- attr(formula, "specials")$rand
-  # Do I care about the environmen, or enclosure for rand_formula, randomise, or treatment??
-  rand_formula <- eval( var_list[[1+rand_index]][[2]])
+  if( length(rand_index)>1){ 
+    stop("there can be only one rand() term in the formula")
+  }
+  rand_expression <- var_list[[1+rand_index]]
+  if( length(rand_expression)!=2 ){
+    stop("' ",deparse(rand_expression), "' must have only one argument, which is a  formula")
+  }
+  if(!grepl("~",deparse(rand_expression[[2]]))){
+    warning("'",deparse(rand_expression[[2]]), "' does not look like a formula")
+  }
+  rand_formula <- eval( rand_expression[[2]])
+  environment(rand_formula) <- env
   rand_formula <- terms(rand_formula)
+  if(!attr(rand_formula,"response")){
+    stop("' ", deparse(rand_expression[[2]]),"' must be a two-sided formula" )
+  }
   formula <- drop.terms(formula, dropx=rand_index-1, keep.response = TRUE)
   randomise <- delete.response(rand_formula)
   treatment <- eval(call("~", attr(rand_formula,"variables")[[2]]))
-  list(formula=outcome, treatment=treatment, randomise=randomise, rand_formula=rand_formula)
+  environment(treatment) <- env
+  list(formula=formula, treatment=treatment, randomise=randomise, rand_formula=rand_formula)
 }
 
 
 test <- one_becomes_three(Surv(progyrs,prog)~entry+rand(I(1-xoyrs/progyrs)~imm))
 test <- one_becomes_three(Surv(progyrs,prog)~entry+rand(imm+def~imm) +age/sex)
+one_becomes_three(y~x+rand(frm,x))
+
 asOneFormula(test)
 
 
