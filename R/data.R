dc084fb2f66809e1b0339ce89c24dd4d29e2ae6f
Simon Bond <sjb277@medschl.cam.ac.uk>
2023-10-27 08:50:19 +0100

    fixing doi link

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' were randomly assigned to receive treatment immediately or deferred. Participants in the deferred 
 #' arm could crossover and receive treatment. The primary endpoint was time to disease progression.
 #' 
-#' The data are based on a randomized controlled trial Concorde \url{http://dx.doi.org/10.1016/S0140-6736(94)90006-X}
+#' The data are based on a randomized controlled trial Concorde \doi{10.1016/S0140-6736(94)90006-X}
 #' \describe{
 #'    \item{id}{participant ID number}
 #'    \item{def}{indicator that the participant was assigned to the Deferred treatment arm}
--- cran-comments.md
+++ cran-comments.md
@@ -1,14 +1,13 @@
 ## Description of Changes
 
-Change contact email.  Modified terms.inner to use inherit().
+Change contact email.  Modified terms.inner to use inherit(). Fixed doi link.
 
 
 ## Test environments
 * Windows 10 PC
-* travis
-* appveyor
 * win-builder
-
+* github
+* r-hub
 
 ## R CMD check results
 There were no ERRORs or WARNINGs or Notes. 
